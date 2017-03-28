71add2e193c7e1d683d77c30613f737ea10e38ae
Ivan <ivangalyulko@gmail.com>
2017-03-28 18:50:44 +0300

    Initial commit

---
+++ example-arrow-functions.js
@@ -0,0 +1,12 @@
+/*var names = ['Vanya', 'Nata', 'Chris'];
+
+names.forEach(function (name) {
+	console.log('forEach', name);
+});
+
+names.forEach((name) => {
+	console.log('arrowFunc', name);
+});
+
+names.forEach((name) => console.log(name));*/
+
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+	entry: './app/app.js',
+	output: {
+		path: __dirname,
+		filename: './public/bundle.js'
+	},
+
+	resolve: {
+		root: __dirname,
+		alias: {
+			Main: 'app/components/Main.js',
+			Nav: 'app/components/Nav.js',
+			Weather: 'app/components/Weather.js',
+			WeatherForm: 'app/components/WeatherForm.js',
+			WeatherMessage: 'app/components/WeatherMessage.js',
+			About: 'app/components/About.js',
+			Examples: 'app/components/Examples.js',
+			openWeatherMap: 'app/api/openWeatherMap.js'
+		},
+		extensions: ['', '.js', '.jsx']
+		
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				query: {
+					presets: ['react', 'es2015']
+				},
+				test: /\.jsx?$/,
+				exlude: /(node_modules|bower_components)/
+			}
+		]
+	}
+};
